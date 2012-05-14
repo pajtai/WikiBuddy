d5a66d293b6f13dbdd1756f0d1c1e39c925e235b
Peter Ajtai <peter.ajtai@gmail.com>
2012-05-13 19:22:02 -0700

    making home.md root

--- Constants.rb
+++ Constants.rb
@@ -4,7 +4,7 @@ module WikiManager
   WIKI_DIR = "../Reference.wiki/"
   WHITE_SPACE = '-'
   VALID_PAGE_RE = /^(.+)\.(md|mkdn?|mdown|markdown|textile|rdoc|org|creole|re?st(\.txt)?|asciidoc|pod|(media)?wiki)$/i
-  ROOT = 'root.md'
+  ROOT = 'home.md'
   SIDEBAR = '_Sidebar.md'
   MAX_DEPTH = 5
   MAX_NODES = 5000
